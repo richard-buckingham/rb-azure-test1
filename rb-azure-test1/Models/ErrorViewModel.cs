39a5c019155f5214eaefbbaf9bd466d7974a7afc
richie.buckingham <richie.buckingham@gmail.com>
2018-08-11 20:57:44 +1000

    init commit

---
+++ rb-azure-test1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace rb_azure_test1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
